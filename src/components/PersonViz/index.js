e5aa19eaac4b34af05a97e2deed5deb12c1d5233
Tyler Martinez <slightlytyler@gmail.com>
2016-03-06 14:20:59 -0600

    Person viz showing savings

--- src/components/PersonViz/index.js
+++ src/components/PersonViz/index.js
@@ -11,6 +11,24 @@ class PersonViz extends Component {
     barData: PropTypes.array.isRequired,
   };
 
+  colors = [
+    colors.taxableIncome,
+    colors.capitalGains,
+    colors.payroll,
+    colors.estate,
+    colors.aca,
+    colors['positive-green'],
+    colors['negative-red'],
+  ];
+
+  colorsFn = idx => this.colors[idx];
+
+  colorAccessor = (series, idx) => (
+    idx === 5 && series.x === 1
+      ? idx + 1
+      : idx
+  );
+
   render() {
     return (
       <div styleName="person-viz">
@@ -18,16 +36,8 @@ class PersonViz extends Component {
           data={this.props.barData}
           width={700}
           height={700}
-          colors={idx => (
-            [
-              colors.taxableIncome,
-              colors.capitalGains,
-              colors.payroll,
-              colors.estate,
-              colors.aca,
-            ][idx]
-          )}
-          colorAccessor={(series, idx) => idx}
+          colors={this.colorsFn}
+          colorAccessor={this.colorAccessor}
         />
       </div>
     );
@@ -41,6 +51,7 @@ import {
   payrollTaxSelector,
   estateTaxSelector,
   acaTaxSelector,
+  totalSavingsSelector,
 } from 'reducers/inputs';
 
 export default connect(
@@ -50,6 +61,17 @@ export default connect(
     const payrollTax = payrollTaxSelector(state);
     const estateTax = estateTaxSelector(state);
     const acaTax = acaTaxSelector(state);
+    const aggregatedTaxes = [
+      ordinaryIncomeTax,
+      capitalGainsTax,
+      payrollTax,
+      estateTax,
+      acaTax,
+    ];
+    const totalCurrent = aggregatedTaxes.reduce((a, b) => (a.current ? a.current : a) + b.current);
+    const totalSanders = aggregatedTaxes.reduce((a, b) => (a.sanders ? a.sanders : a) + b.sanders);
+    const totalSavings = totalSavingsSelector(state);
+    const positiveTotalSavings = totalSavings >= 0;
 
     return {
       barData: [
@@ -86,6 +108,13 @@ export default connect(
           values: [
             { x: 1, y: acaTax.current },
             { x: 2, y: acaTax.sanders },
+          ],
+        },
+        {
+          name: 'Savings',
+          values: [
+            { x: 1, y: positiveTotalSavings ? 0 : Math.abs(totalCurrent - totalSanders) },
+            { x: 2, y: positiveTotalSavings ? Math.abs(totalSavings) : 0 },
           ],
         },
       ],
