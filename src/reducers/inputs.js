8870a8f63f4d969e4848bcc787cdb1fe97c30f1c
Tyler Martinez <slightlytyler@gmail.com>
2016-03-05 18:23:26 -0600

    Base selectors

--- src/reducers/inputs.js
+++ src/reducers/inputs.js
@@ -1,6 +1,43 @@
 // Constants
 const UPDATE_INPUTS = 'UPDATE_INPUTS';
 
+// Selectors
+import { createSelector } from 'reselect';
+
+export const inputsSelector = state => state.input;
+export const taxableIncomeSelector = createSelector(
+  inputsSelector,
+  inputs => inputs.taxableIncome
+);
+export const filingStateSelector = createSelector(
+  inputsSelector,
+  inputs => inputs.filingStatus
+);
+export const agiSelector = createSelector(
+  inputsSelector,
+  inputs => inputs.agi
+);
+export const capitalGainsSelector = createSelector(
+  inputsSelector,
+  inputs => inputs.capitalGains
+);
+export const estateBenefitsSelector = createSelector(
+  inputsSelector,
+  inputs => inputs.estateBenefits
+);
+export const monthlyInsurancePremiumSelector = createSelector(
+  inputsSelector,
+  inputs => inputs.monthlyInsurancePremium
+);
+export const insuranceDeductibleSelector = createSelector(
+  inputsSelector,
+  inputs => inputs.insuranceDeductible
+);
+export const anticipatedYearlyHealthSpendingSelector = createSelector(
+  inputsSelector,
+  inputs => inputs.anticipatedYearlyHealthSpending
+);
+
 // Actions
 export const actions = {
   updateInputs: (key, val) => ({ type: UPDATE_INPUTS, key, val }),
