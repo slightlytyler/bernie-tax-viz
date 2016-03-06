484ad1dd837dcca1d9af956690c71de0ee2505f6
Tyler Martinez <slightlytyler@gmail.com>
2016-03-06 13:42:03 -0600

    Bar chart on page

--- src/components/PersonViz/index.js
+++ src/components/PersonViz/index.js
@@ -1,13 +1,69 @@
 import React, { Component } from 'react';
 import cssModules from 'react-css-modules';
+import { BarChart } from 'react-d3';
+
+import colors from 'styles/colors';
 import styles from './styles.styl';
 
 @cssModules(styles, { allowMultiple: true, errorWhenNotFound: false })
-export default class MainVizPersonViz extends Component {
+export default class PersonViz extends Component {
+  barData = [
+    {
+      name: 'Taxable Income',
+      color: colors.taxableIncome,
+      values: [
+        { x: 1, y: 91 },
+        { x: 2, y: 150 }
+      ],
+    },
+    {
+      name: 'Capital Gains',
+      values: [
+        { x: 1, y: 91 },
+        { x: 2, y: 150 }
+      ],
+    },
+    {
+      name: 'Payroll Tax',
+      values: [
+        { x: 1, y: 91 },
+        { x: 2, y: 150 }
+      ],
+    },
+    {
+      name: 'Estate Benefits',
+      values: [
+        { x: 1, y: 91 },
+        { x: 2, y: 150 }
+      ],
+    },
+    {
+      name: 'ACA Tax',
+      values: [
+        { x: 1, y: 91 },
+        { x: 2, y: 150 }
+      ],
+    },
+  ];
+
   render() {
     return (
       <div styleName="person-viz">
-        <div>person viz</div>
+        <BarChart
+          data={this.barData}
+          width={500}
+          height={200}
+          colors={idx => (
+            [
+              colors.taxableIncome,
+              colors.capitalGains,
+              colors.payroll,
+              colors.estate,
+              colors.aca
+            ][idx]
+          )}
+          colorAccessor={(series, idx) => idx}
+        />
       </div>
     );
   }
--- src/styles/colors.js
+++ src/styles/colors.js
@@ -3,4 +3,9 @@ export default {
   white: '#FAFAFA',
   'negative-red': '#E57373',
   'positive-green': '#00E676',
+  taxableIncome: '#FF9100',
+  capitalGains: '#00B0FF',
+  payroll: '#1DE9B6',
+  estate: '#FF80AB',
+  aca: '#EA80FC',
 };
