c6e3c4fdfeb9a9965a57aae657724c0ebdfcd241
dywtkas <jacob.hausler@gmail.com>
2016-03-06 23:16:57 -0600

    updates to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,12 +9,32 @@ export default class Footer extends Component {
       <footer className="pane" styleName="footer">
         <span>The Plan by dataviz.work</span>
         <span>information sourced from:</span>
+        <br>
         <a
           href="http://taxpolicycenter.org/UploadedPDF/2000639-an-analysis-of-senator-bernie-sanderss-tax-proposals.pdf"
           styleName="link"
         >
           Tax Policy Center
-        </a>
+        </a><br>
+        <a
+          href="http://http://berniecare.org/"
+          styleName="link"
+        >
+          berniecare.org
+        </a><br>
+        <a
+          href="http://http://bernietax.com/"
+          styleName="link"
+        >
+          bernietax.com
+        </a><br>
+        IRS <br>
+        SSA <br>
+        <br>
+          Figures provided are solely estimates and are not tax advice.
+        <br><br>
+          Please email admin@dataviz.work with comments and suggestions.
+
       </footer>
     );
   }
