e061154bc1ab2dc57ba7ed0764410430b1fc1d30
Tyler Martinez <slightlytyler@gmail.com>
2016-03-06 22:34:37 -0600

    Footer component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,13 +6,10 @@ import vector from 'assets/bernie-vector.svg';
 import text from 'assets/text.svg';
 
 @cssModules(styles, { allowMultiple: true, errorWhenNotFound: false })
-export default class Title extends Component {
-  static propTypes ={
-  };
-
+export default class Header extends Component {
   render() {
     return (
-      <header styleName="title">
+      <header styleName="header">
         <img src={text} styleName="text" />
         <img src={vector} styleName="vector" />
       </header>
