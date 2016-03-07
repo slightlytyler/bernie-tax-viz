d350d4dfdb109771b06c8d92d864a808fcb501c7
Jacob Hausler <you@example.com>
2016-03-06 21:10:52 -0600

    Updated with top-text of instructions

--- src/components/Instructions/index.js
+++ src/components/Instructions/index.js
@@ -11,7 +11,10 @@ export default class Title extends Component {
   render() {
     return (
       <div className="pane" styleName="instructions">
-        Instructions go here.
+        The Tax Plan with Bernie Sanders shows how much more or less taxes Americans
+        will personally pay under the Bernie Sanders Tax Plan. First, meet the taxpayers,
+        and see how different test cases will fare under The Plan. Second, input your own particulars.
+        Then, see your personalized results under the The Plan. Finally, share a test case with your friends to spread the word!
       </div>
     );
   }
