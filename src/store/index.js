b8c4f036128d32ac37bf6ca2d1cbe5552d76b2ec
Tyler Martinez <slightlytyler@gmail.com>
2016-03-07 18:15:08 -0600

    Persistence working

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,20 @@
 import { applyMiddleware, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
+import * as storage from 'redux-storage';
 import rootReducer from 'reducers';
+
+const reducer = storage.reducer(rootReducer);
+
+import createEngine from 'redux-storage-engine-localstorage';
+const engine = createEngine('my-sav');
+
+const storageMiddleware = storage.createMiddleware(engine);
+
+const load = storage.createLoader(engine);
 
 export default function configureStore(initialState = {}, routerMiddleware) {
   // Compose final middleware and use devtools in debug environment
-  let middleware = applyMiddleware(thunk, routerMiddleware);
+  let middleware = applyMiddleware(thunk, routerMiddleware, storageMiddleware);
   if (__DEBUG__) {
     const devTools = window.devToolsExtension
       ? window.devToolsExtension()
@@ -15,7 +25,7 @@ export default function configureStore(initialState = {}, routerMiddleware) {
   }
 
   // Create final store and subscribe router in debug env ie. for devtools
-  const store = middleware(createStore)(rootReducer, initialState);
+  const store = middleware(createStore)(reducer, initialState);
 
   if (module.hot) {
     module.hot.accept('reducers', () => {
@@ -24,6 +34,8 @@ export default function configureStore(initialState = {}, routerMiddleware) {
       store.replaceReducer(nextRootReducer);
     });
   }
+
+  load(store);
 
   return store;
 }
